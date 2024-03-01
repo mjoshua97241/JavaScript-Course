2d6ef1a13a1ad7d9c9a7a19bac9cfcea80d7cbfe
mjoshua97241 <vargas.michaeljoshua@gmail.com>
2024-03-01 16:05:23 +0800

    Top Level Await

--- 17-Modern-JS-Modules-Tooling/script.js
+++ 17-Modern-JS-Modules-Tooling/script.js
@@ -2,7 +2,7 @@
 // import { addToCart, totalPrice as price, qt } from './shoppingCart.js';
 // addToCart('bread', 5);
 // console.log(price, qt);
-console.log('Importing module');
+console.log("Importing module");
 
 // Import EVERYTHING into an object
 // import * as ShoppingCart from './shoppingCart.js';
@@ -13,9 +13,34 @@ console.log('Importing module');
 // import add, { addToCart, totalPrice as price, qt } from './shoppingCart.js';
 
 //?NOTE: Import is a LIVE connection from exports.
-import add, { cart } from './shoppingCart.js';
-add('pizza', 2);
-add('bread', 5);
-add('apples', 4);
+import add, { cart } from "./shoppingCart.js";
+add("pizza", 2);
+add("bread", 5);
+add("apples", 4);
 
 console.log(cart); //imports are NOT copies of the exports
+
+///* TOP LEVEL AWAIT
+// await can be use and working outside the async function but it should be use only on module (in index.html script type)
+///?NOTE: BLOCKS the entire execution of this module. This can be useful, and can be harmful when running a long task.
+// console.log("Start fetching");
+// const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+// const data = await res.json();
+// console.log(data);
+// console.log("Something");
+
+const getLastPost = async function () {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+console.log(lastPost);
+
+// NOT very clean
+// lastPost.then((last) => console.log(last));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
--- 17-Modern-JS-Modules-Tooling/shoppingCart.js
+++ 17-Modern-JS-Modules-Tooling/shoppingCart.js
@@ -1,5 +1,10 @@
 // Exporting module
-console.log('Exporting module');
+console.log("Exporting module");
+
+// Blocking code
+// console.log("Start fetching users");
+// const res = await fetch("https://jsonplaceholder.typicode.com/users");
+// console.log("Finish fetching users");
 
 const shippingCost = 10;
 export const cart = [];
