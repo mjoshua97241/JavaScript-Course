16ffbcca33a9a1c4f116ba58a440ddde14be0ef5
mjoshua97241 <vargas.michaeljoshua@gmail.com>
2024-03-09 18:26:47 +0800

    Fix 02

--- 17-Modern-JS-Modules-Tooling/clean.js
+++ 17-Modern-JS-Modules-Tooling/clean.js
@@ -1,4 +1,6 @@
-const budget = [
+"strict mode";
+
+const budget = Object.freeze([
   { value: 250, description: "Sold old TV 📺", user: "jonas" },
   { value: -45, description: "Groceries 🥑", user: "jonas" },
   { value: 3500, description: "Monthly salary 👩‍💻", user: "jonas" },
@@ -7,18 +9,23 @@ const budget = [
   { value: -20, description: "Candy 🍭", user: "matilda" },
   { value: -125, description: "Toys 🚂", user: "matilda" },
   { value: -1800, description: "New Laptop 💻", user: "jonas" },
-];
+]);
 
-const spendingLimits = {
+// Object.freeze - make the data immutable (no longer put properties). This only freezes ONE LEVEL only. This is NOT A DEEP FREEZE.
+const spendingLimits = Object.freeze({
   jonas: 1500,
   matilda: 100,
-};
+});
+// spendingLimits.jay = 200;
+console.log(spendingLimits);
 
-const getLimit = (user) => spendingLimits?.[user] ?? 0;
+// const lim = spendingLimits[user] ? spendingLimits[user] : 0;
+const getLimit = (limits, user) => limits?.[user] ?? 0;
 
-const addExpense = function (value, description, user = "jonas") {
+// PURE FUNCTION :D
+const addExpense = function (state, limit, value, description, user = "jonas") {
   // if (!user) user = "jonas"; //make it a default
-  user = user.toLowerCase();
+  const cleanUser = user.toLowerCase();
 
   // Problem
   // let limit;
@@ -31,39 +38,52 @@ const addExpense = function (value, description, user = "jonas") {
   // const lim = spendingLimits[user] ? spendingLimits[user] : 0;
 
   // Solution #2 - Optional Chaining
-  const limit = getLimit(user);
+  // const limit = getLimit(user);
 
   // Problem
   // if (value <= limit) {
   //   budget.push({ value: -value, description: description, user: user });
   // }
-  if (value <= getLimit(user)) {
-    budget.push({ value: -value, description, user });
-  }
+  return value <= getLimit(limit, cleanUser)
+    ? // creates a copy of object, then add elements
+      [...state, { value: -value, description, user: cleanUser }]
+    : state;
+
+  // budget.push({ value: -value, description, user: cleanUser });
 };
-addExpense(10, "Pizza 🍕");
-addExpense(100, "Going to movies 🍿", "Matilda");
-addExpense(200, "Stuff", "Jay");
+const newBudget1 = addExpense(budget, spendingLimits, 10, "Pizza 🍕");
 
-const checkExpenses = function () {
-  for (const entry of budget) {
-    // Problem
-    // let limit;
-    // if (spendingLimits[entry.user]) {
-    //   limit = spendingLimits[entry.user];
-    // } else {
-    //   limit = 0;
-    // }
-    // Solution 1
-    // const limit = spendingLimits?.[entry.user] ?? 0;
+const newBudget2 = addExpense(
+  newBudget1, //to add the result from newBudget1
+  spendingLimits,
+  100,
+  "Going to movies 🍿",
+  "Matilda"
+);
+const newBudget3 = addExpense(newBudget2, spendingLimits, 200, "Stuff", "Jay");
 
-    // Solution 2
-    if (entry.value < -getLimit(entry.user)) entry.flag = "limit";
-  }
+const checkExpenses = function (state, limits) {
+  state.map();
+
+  // for (const entry of newBudget3) {
+  // Problem
+  // let limit;
+  // if (spendingLimits[entry.user]) {
+  //   limit = spendingLimits[entry.user];
+  // } else {
+  //   limit = 0;
+  // }
+  // Solution 1
+  // const limit = spendingLimits?.[entry.user] ?? 0;
+
+  // Solution 2
+  // if (entry.value < -getLimit(limits, entry.user)) entry.flag = "limit";
+  // }
 };
-checkExpenses();
+checkExpenses(newBudget3, spendingLimits);
+console.log(newBudget3);
 
-console.log(budget);
+// console.log(budget);
 
 const logBigExpenses = function (bigLimit) {
   let output = "";
@@ -80,5 +100,5 @@ const logBigExpenses = function (bigLimit) {
   console.log(output);
 };
 
-console.log(budget);
+// console.log(budget);
 logBigExpenses(500);
