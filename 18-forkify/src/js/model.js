5249535b50d34b042c04ef64b659ca2fa6960a39
mjoshua97241 <vargas.michaeljoshua@gmail.com>
2024-03-17 06:22:30 +0800

    Refactoring MVC 01

--- 18-forkify/src/js/model.js
+++ 18-forkify/src/js/model.js
@@ -1,32 +1,33 @@
-export const state {
-    recipe: {},
-}
+import { async } from 'regenerator-runtime';
+
+export const state = {
+  recipe: {},
+};
 
 export const loadRecipe = async function (id) {
-    try {
+  try {
+    const res = await fetch(
+      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc3e'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+    );
+    const data = await res.json();
 
-        const res = await fetch(
-            // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc3e'
-            `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-            );
-            const data = await res.json();
-            
-            if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-            
-      console.log(res, data);
-      const { recipe } = data.data;
-      state.recipe = {
-          id: recipe.id,
-          title: recipe.title,
-          publisher: recipe.publisher,
-          sourceUrl: recipe.source_url,
-          image: recipe.image_url,
-          servings: recipe.servings,
-          cookingTime: recipe.cooking_time,
-          ingredients: recipe.ingredients,
-        };
-        console.log(state.recipe);
-    } catch (err) {
-        alert(err)
-    }
-}
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    console.log(res, data);
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
