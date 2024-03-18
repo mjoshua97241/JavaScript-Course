6ec290b1aa0d7ddd75ba21fb672ba0b56e90e8dd
mjoshua97241 <vargas.michaeljoshua@gmail.com>
2024-03-18 16:01:13 +0800

    Refactoring for MVC

--- 18-forkify/src/js/views/recipeView.js
+++ 18-forkify/src/js/views/recipeView.js
@@ -88,23 +88,7 @@ class RecipeView {
       <div class="recipe__ingredients">
         <h2 class="heading--2">Recipe ingredients</h2>
         <ul class="recipe__ingredient-list">
-          ${this.#data.ingredients
-            .map(ing => {
-              return `
-            <li class="recipe__ingredient">
-              <svg class="recipe__icon">
-                <use href="${icons}#icon-check"></use>
-              </svg>
-              <div class="recipe__quantity">${new Fraction(
-                ing.quantity
-              ).toString()}</div>
-              <div class="recipe__description">
-                <span class="recipe__unit">${ing.unit}</span>
-                ${ing.description}
-              </div>
-            </li>`;
-            })
-            .join('')}
+          ${this.#data.ingredients.map(this.#generateMarkupIngredient).join('')}
         </ul>
       </div>
 
@@ -129,6 +113,24 @@ class RecipeView {
         </a>
       </div>
   `;
+  }
+
+  #generateMarkupIngredient(ing) {
+    ing => {
+      return `
+    <li class="recipe__ingredient">
+      <svg class="recipe__icon">
+        <use href="${icons}#icon-check"></use>
+      </svg>
+      <div class="recipe__quantity">${
+        ing.quantity ? new Fraction(ing.quantity).toString() : ''
+      }</div>
+      <div class="recipe__description">
+        <span class="recipe__unit">${ing.unit}</span>
+        ${ing.description}
+      </div>
+    </li>`;
+    };
   }
 }
 export default new RecipeView();
