ad04716b2626602e6078bf1746187704c621fd8c
mjoshua97241 <vargas.michaeljoshua@gmail.com>
2024-03-28 19:49:13 +0800

    pagination

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -8,11 +8,10 @@ import resultsView from './views/resultsView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-import { async } from 'regenerator-runtime';
 
-if (module.hot) {
-  module.hot.accept();
-}
+// if (module.hot) {
+//   module.hot.accept();
+// }
 
 const recipeContainer = document.querySelector('.recipe');
 
@@ -50,8 +49,9 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // 3) Render results
-    console.log(model.state.search.results);
-    resultsView.render(model.state.search.results);
+    // resultsView.render(model.state.search.results);
+
+    resultsView.render(model.getSearchResultsPage());
   } catch (err) {
     console.log(err);
   }
--- 18-forkify/src/js/model.js
+++ 18-forkify/src/js/model.js
@@ -1,12 +1,15 @@
 import { async } from 'regenerator-runtime';
-import { API_URL } from './config.js';
+import { API_URL, RES_PER_PAGE } from './config.js';
 import { getJSON } from './helpers.js';
 
 export const state = {
   recipe: {},
-  search: {},
-  query: '',
-  results: [],
+  search: {
+    query: '',
+    results: [],
+    page: 1,
+    resultsPerPage: RES_PER_PAGE,
+  },
 };
 
 export const loadRecipe = async function (id) {
@@ -51,4 +54,13 @@ export const loadSearchResults = async function (query) {
     console.error(`${err} 💥💥💥💥`);
     throw err;
   }
+};
+
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+
+  const start = (page - 1) * state.search.resultsPerPage; // 0;
+  const end = page * state.search.resultsPerPage; // 9;
+
+  return state.search.results.slice(start, end);
 };
