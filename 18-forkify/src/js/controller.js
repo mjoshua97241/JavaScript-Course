b069d1b0cf46c8a31f658cac8a18de4f6269c123
mjoshua97241 <vargas.michaeljoshua@gmail.com>
2024-04-09 16:33:47 +0800

    Bookmarks 02

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -4,6 +4,7 @@ import searchView from './views/searchView.js';
 import SearchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
 
 // import icons from '../img/icons.svg'; // Parcel 1
 
@@ -30,6 +31,7 @@ const controlRecipes = async function () {
 
     // 0) Update results view to mark selected search result
     resultsView.update(model.getSearchResultsPage());
+    bookmarksView.update(model.state.bookmarks);
 
     // 1 Loading Recipe
     await model.loadRecipe(id);
@@ -84,11 +86,16 @@ const controlServings = function (newServings) {
 };
 
 const controlAddBookmark = function () {
+  // 1) Add or remove bookmark
+
   if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
   else model.deleteBookmark(model.state.recipe.id);
 
-  console.log(model.state.recipe);
+  //  2) Update recipe view
   recipeView.update(model.state.recipe);
+
+  // 3) Render bookmarks
+  bookmarksView.render(model.state.bookmarks);
 };
 
 const init = function () {
