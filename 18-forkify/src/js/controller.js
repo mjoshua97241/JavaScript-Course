b089f437abec817735f637f800894b410fe909a9
mjoshua97241 <vargas.michaeljoshua@gmail.com>
2024-04-22 12:22:45 +0800

    Added new welcome feature

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -139,6 +139,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -147,5 +151,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
